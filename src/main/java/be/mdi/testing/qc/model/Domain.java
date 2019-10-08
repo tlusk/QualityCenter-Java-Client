109f256b04638f51fc01655256ebadae8c6481d5
Dirickx <Matthias.Dirickx@ordina.be>
2019-10-08 15:09:11 +0200

    Changed Domain class and added test for Domains.

--- src/main/java/be/mdi/testing/qc/model/Domain.java
+++ src/main/java/be/mdi/testing/qc/model/Domain.java
@@ -1,19 +1,25 @@
 package be.mdi.testing.qc.model;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "Domain")
 public class Domain {
 
     private String name;
+    private Projects projects;
 
     @XmlAttribute(name = "Name")
     public String getName() {
         return name;
     }
 
+    @XmlElement(name = "Projects", required = false)
+    public Projects getProjects() { return projects; }
+
     public void setName(String name) {
         this.name = name;
     }
+    public void setProjects(Projects projects) { this.projects = projects; }
 }
--- src/test/java/DomainsTest.java
+++ src/test/java/DomainsTest.java
@@ -68,4 +68,36 @@ public class DomainsTest {
 
         mockServer.stop();
     }
+
+    //TODO write test so that the domain names are put to a list when there's projects included.
+    @Test
+    public void theMethodCanGenerateAnObjectFromAValidXmlForThreeDomainsAndPrintFullListWithProjects() {
+        List<String> listToVerifyAgainst = new ArrayList<String>();
+        listToVerifyAgainst.add("Domain_NAME_1");
+        listToVerifyAgainst.add("Domain_NAME_2");
+        listToVerifyAgainst.add("Domain_NAME_3");
+
+        mockServer = startClientAndServer(1080);
+        mockServer
+                .when(request("/qcbin/rest/domains"))
+                .respond(response()
+                        .withHeader("Content-Type", "application/xml")
+                        .withCookie("some-cookie", "to avoid default")
+                        .withBody("<Domains>\n" +
+                                "       <Domain Name=\"Domain_NAME_1\"/>\n" +
+                                "       <Domain Name=\"Domain_NAME_2\"/>\n" +
+                                "       <Domain Name=\"Domain_NAME_3\"/>\n" +
+                                "</Domains>"));
+
+        QCRestClient qcc = new QCRestClient("http://127.0.0.1:1080", "abc", "def");
+        Domains domains = qcc.getDomains();
+
+        System.out.println(domains.getNames().toString());
+        assert domains.getNames().equals(listToVerifyAgainst);
+
+        mockServer.stop();
+    }
+
+    //TODO write test that checks if domains and properties are hierarchically correctly represented.
+
 }
